54bf3fb0b3f249c8133b8fe2f1595b68383a8fbd
Unknown <wolf_digit@hotmail.com>
2018-03-21 13:53:59 +0800

    typing score

---
+++ php/index1061.php
@@ -0,0 +1,132 @@
+<?php 
+$path="";
+require_once('util.inc.php');
+?>
+<?php 
+    $title="老狼的資科概";
+    $active=1;
+    require( 'header.inc.php' ); 
+?>
+    <!-- Header -->
+    <header class="masthead" style="background: url('https://unsplash.it/1900/1080?image=668') no-repeat center center scroll">
+    <div class="overlay">
+      <div class="container">
+        <h1 class="display-1 text-white">資訊科技概論</h1>
+        <h2 class="display-4 text-white">106學年度‧上學期</h2>
+        <p class="text-white"><del>立志成為最潮的課程網站(?)</del></p>
+      </div>
+    </div>
+  </header>
+
+  <!-- main -->
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 order-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="https://unsplash.it/500/500?image=445" alt="">
+          </div>
+        </div>
+        <div class="col-md-6 order-1">
+          <div class="p-5">
+            <h2 class="display-4">麗山與它的資訊系統們</h2>
+            <p>要成為一個合格的麗山人，你至少要會收信吧...?</p>
+            <p>線上選社呢?查成績呢?</p>
+            <p>課程網站怎麼進去? &nbsp; &nbsp; --欸不是只有資訊科會用課程網站喔</p>
+            <p>什麼!? 課程網站有分三種!?</p>
+            <p>老師對不起我又忘記密碼了...orz</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 order-1">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="https://unsplash.it/500/500?image=403" alt="">
+          </div>
+        </div>
+        <div class="col-md-6 order-2">
+          <div class="p-5">
+            <h2 class="display-4">英文打字練習</h2>
+            <p>天下武功 唯快不破</p>
+            <p>要把資訊科學好，就要先練成打字神速無影手！<br>
+              (大概啦?)</p>
+            <p>打字測驗程式 - <a href="https://drive.google.com/open?id=0B65DH-sSwkNARVJjbHVZSlZxc00">WinWTS</a></p>
+            <p>f(n) = 2*n + 30 ， 測5次取較高分的3次平均</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 order-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="https://images.unsplash.com/photo-1455390582262-044cdead277a?dpr=2&auto=format&fit=scale&w=300&h=300&q=80" alt="">
+          </div>
+        </div>
+        <div class="col-md-6 order-1">
+          <div class="p-5">
+            <h2 class="display-4">部落格寫作</h2>
+            <p>這是網路訊息爆炸的時代</p>
+            <p>充滿直播、影片、照片，或者只有一句話的即時訊息</p>
+            <p>想當年無名小站還在的時候...我們是怎麼思考的...?</p>
+            <p>(幾乎)每週一篇部落格文章，400字以上原創文字，嚴禁抄襲</p>
+            <p>如果是閱讀文章/新聞產生的心得，請附參考資料連結</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 order-1">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="https://unsplash.it/500/500?image=371" alt="">
+          </div>
+        </div>
+        <div class="col-md-6 order-2">
+          <div class="p-5">
+            <h2 class="display-4">旅遊計畫書</h2>
+            <p>放心吧其實沒有真的要出去玩 :p</p>
+            <p>學會使用google文件、試算表、簡報等等服務，讓小組合作更方便。</p>
+            <p>當然還要有精美排版啊</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6 order-2">
+            <div class="p-5">
+              <!--<img class="img-fluid rounded-circle" src="https://unsplash.it/500/500?image=1" alt="">-->
+              <img class="img-fluid rounded-circle" src="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?dpr=1&auto=format&fit=crop&w=500&h=500&q=80&cs=tinysrgb&crop=" alt="">
+            </div>
+          </div>
+          <div class="col-md-6 order-1">
+            <div class="p-5">
+              <h2 class="display-4">C語言程式設計入門</h2>
+              <p>學習程式設計不是要每個人都成為軟體工程師，而是訓練系統化解決問題的方法和邏輯思維</p>
+              <p><del>好課值得一修再修(?)</del></p>
+              <p>很入門很入門</p>
+              <p>但是還是很可怕很可怕</p>
+              <p>哎呀反正你就專心用力把它聽懂，你就已經贏過全國87%(?)的人了<br>
+                真的！</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <?php require( 'footer.inc.php' ); ?>
